0c796504be15ed1c048b5ce672d0109304874fa8
Artem <Azumishinji.jp@gmail.com>
2022-02-02 17:16:46 +0300

    New examples
    ++ 4.2
    ++ 4.3
    ++ 4.4

--- 4.2 strings/strings.cpp
+++ 4.2 strings/strings.cpp
@@ -15,7 +15,12 @@ int main()
 	cout << "! What's your name?\n";
 	cin >> name1;
 	cout << "Well, " << name1 << ", your name has ";
-	cout << strlen(name1) << " letters and is stired\n";
+	cout << strlen(name1) << " letters and is stored\n";
 	cout << "in an array of " << sizeof(name1) << " bytes.\n";
-	cout << "Your initial is " << name[0]
+	cout << "Your initial is " << name1[0] << ".\n";
+	name2[3] = '\0';				// установка нулевого символа
+	cout << "Here are the first 3 characters of my name: ";
+	cout << name2 << endl;
+
+	return 0;
 }
