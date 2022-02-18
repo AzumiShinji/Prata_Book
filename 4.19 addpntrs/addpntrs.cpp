20a50e6af3c5c33da84f7b13c99ee67e91e40bca
Artem <Azumishinji.jp@gmail.com>
2022-02-18 08:37:05 +0300

    new ex

---
+++ 4.19 addpntrs/addpntrs.cpp
@@ -0,0 +1,10 @@
+// addpntrs.cpp -- сложение указателей
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	double wages[3] = { 10000.0, 20000.0, 30000.0 };
+	short stacks[3] = { 3,2,1 };
+}
